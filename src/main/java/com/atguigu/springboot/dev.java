a7c95a690e24453f467fbbd7743e45c36c13bdad
yxk0620 <441732552@qq.com>
2020-10-24 18:44:08 +0800

    分支修改666

--- src/main/java/com/atguigu/springboot/dev.java
+++ src/main/java/com/atguigu/springboot/dev.java
@@ -3,6 +3,6 @@ package com.atguigu.springboot;
 public class dev {
     public static void main(String[] args) {
         System.out.println("分支");
-        System.out.println("分支修改");
+        System.out.println("分支修改666");
     }
 }
